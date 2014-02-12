5344e6b4f881e57e1ee21fe3df83cdd550aa5652
Yiyang Song <songyiyang2012@gmail.com>
2014-02-12 11:38:39 -0500

    add unique words

--- lib/first_gem_yiyang_song.rb
+++ lib/first_gem_yiyang_song.rb
@@ -1,5 +1,11 @@
 require "first_gem_yiyang_song/version"
 
-module FirstGemYiyangSong
-  # Your code goes here...
+class String
+	def word_count
+		self.split.count
+	end
+
+	def unique_words
+		sel.split.uniq
+	end
 end
